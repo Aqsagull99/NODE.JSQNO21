14cca7ebf43c0a68f62d5e86b1030055c9e12338
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 09:31:43 +0500

    Add files via upload

---
+++ QNO21.ts
@@ -0,0 +1,26 @@
+// QNO:21
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+
+//  type books={name:string,pages:number,price:number,}
+
+
+// let English:books={
+//     name:"OXFORD",
+//     price:530,
+//     pages:400
+  
+// }
+
+// let comerce:books={
+//     name:"commerce",
+//     price:600,
+//     pages:200
+// }
+
+// let scince:books={
+//     name:"Science",
+//     price:500,
+//     pages:600
+// }
+// console.log(English,comerce,scince.price )
